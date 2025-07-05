de181c6e1f9cc5176cb4cfa3dba4132de71a3ce3
Chinmay <chinmayjindal108@gmail.com>
2025-07-04 23:35:03 -0400

    update info

--- info.py
+++ info.py
@@ -3,10 +3,6 @@ nums = [] # upload numbers you want to send here, in E.164 format
 my_number = "" # upload twilio number here, found from account dashboard
 
 text = (
-    "Hello! This is a reminder that Aadya's Arangetram will take place tomorrow, Saturday, July 5.\n\n"
-    "📍 Location: City Playhouse Theatre\n"
-    "🍴 Refreshments: 3:00PM\n"
-    "🎭 Performance Start: 4:00PM\n"
-    "🍽️ Please join us for dinner after the event.\n\n"
-    "We are looking forward to your presence!"
+    "text block 1\n\n"
+    "text block 2\n"
 )
